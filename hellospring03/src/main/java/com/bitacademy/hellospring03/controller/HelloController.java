7a2985130dca408378727590237d964c872e242f
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2020-12-30 15:28:17 +0900

    Spring MVC 연습#1

--- hellospring03/src/main/java/com/bitacademy/hellospring03/controller/HelloController.java
+++ hellospring03/src/main/java/com/bitacademy/hellospring03/controller/HelloController.java
@@ -1,7 +1,13 @@
 package com.bitacademy.hellospring03.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -9,4 +15,32 @@ public class HelloController {
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
 	}
+	
+	@RequestMapping("/hello2")
+	public String hello2(String name) {
+		System.out.println(name);
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(String name) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("name", name);
+		mav.setViewName("/WEB-INF/views/hello.jsp");
+		
+		return mav;
+	}
+	
+	@RequestMapping("/hello4")
+	public String hello4(String name, Model model) {
+		model.addAttribute("name", name);
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+	@RequestMapping("/hello5")
+	@ResponseBody
+	public String hello5(String name) {
+		return "Hello " + name;
+	}
+	
 }
