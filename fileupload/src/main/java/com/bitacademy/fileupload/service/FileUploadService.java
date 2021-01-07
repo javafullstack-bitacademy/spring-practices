c1609c1612c82181a433b8e17c6fc04ebef2bf40
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2021-01-07 15:36:56 +0900

    fileupload #2

--- fileupload/src/main/java/com/bitacademy/fileupload/service/FileUploadService.java
+++ fileupload/src/main/java/com/bitacademy/fileupload/service/FileUploadService.java
@@ -1,13 +1,18 @@
 package com.bitacademy.fileupload.service;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Calendar;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileUploadService {
-
+	private static final String SAVE_PATH = "/mysite-uploads";
+	private static final String URL_BASE = "/images";
+	
 	public String restore(MultipartFile multipartFile) {
 		String url = "";
 		
@@ -23,6 +28,11 @@ public class FileUploadService {
 			System.out.println("#############" + fileSize);
 			
 			byte[] fileData = multipartFile.getBytes();
+			OutputStream os = new FileOutputStream(SAVE_PATH + "/" + saveFilename);
+			os.write(fileData);
+			os.close();
+			
+			url = URL_BASE + "/" + saveFilename;
 			
 		} catch (IOException e) {
 			throw new RuntimeException("file upload error:" + e);
@@ -34,7 +44,14 @@ public class FileUploadService {
 	private String genSaveFilename(String extName) {
 		String filename = "";
 		
-		
+		Calendar calendar = Calendar.getInstance();
+		filename += calendar.get(Calendar.YEAR);
+		filename += calendar.get(Calendar.MONTH);
+		filename += calendar.get(Calendar.DATE);
+		filename += calendar.get(Calendar.HOUR);
+		filename += calendar.get(Calendar.MINUTE);
+		filename += calendar.get(Calendar.SECOND);
+		filename += calendar.get(Calendar.MILLISECOND);
 		filename += ("." + extName);
 		return filename;
 	}
